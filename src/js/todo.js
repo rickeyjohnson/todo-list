2a5d4e3a6ab03a28afc5f16fb09f16bd252122f2
rickeyjohnson <rdjwinter2004@gmail.com>
2024-09-03 00:52:08 -0400

    added date-fns, and create basic todo object

---
+++ src/js/todo.js
@@ -0,0 +1,10 @@
+import { format } from "date-fns"
+
+const todo = () => {
+    let title = this.title
+    let description = this.description
+    let dueDate = new Date(this.year, this.month, this.day)
+    let priority = this.priority
+    let project = this.project
+    let status = this.status
+}
