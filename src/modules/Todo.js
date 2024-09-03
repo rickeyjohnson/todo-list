77e3fca9d42b5641cfa97cfa2123f4b2e9157b7e
rickeyjohnson <rdjwinter2004@gmail.com>
2024-09-03 17:19:48 -0400

    created Todo class with constructer and edit "setter" methods

---
+++ src/modules/Todo.js
@@ -0,0 +1,20 @@
+class Todo {
+    constructor (title, description, project, completed = false) {
+        this.title = title
+        this.description = description
+        this.project = project
+        this.completed = completed
+    }
+
+    editTitle(nTitle) {
+        this.title = nTitle
+    }
+
+    editDescription(nDescription) {
+        this.description = nDescription
+    }
+
+    editCompleted(nCompleted) {
+        this.completed = nCompleted
+    }
+}
