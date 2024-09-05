74fdcef636fc66fb71f25ead3a8cfa1f3e98aa09
rickeyjohnson <rdjwinter2004@gmail.com>
2024-09-05 13:52:56 -0400

    added some more helper functions, the storage available, and added morefunctions in DOM.js

--- src/js_components/helperFunctions.js
+++ src/js_components/helperFunctions.js
@@ -5,5 +5,24 @@ function createElementWithIdAndClass(tag, id="", className="") {
     return element
 }
 
-export { createElementWithIdAndClass }
+function storageAvailable(type) {
+    let storage;
+    try {
+      storage = window[type];
+      const x = "__storage_test__";
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return (
+        e instanceof DOMException &&
+        e.name === "QuotaExceededError" &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        storage &&
+        storage.length !== 0
+      );
+    }
+}
+
+export { createElementWithIdAndClass, storageAvailable }
   
--- src/views/DOM.js
+++ src/views/DOM.js
@@ -1,6 +1,6 @@
 import { Dashboard } from '../modules/Dashboard'
 import { Project } from '../modules/Project'
-import { createElementWithIdAndClass } from '../js_components/helperFunctions'
+import { createElementWithIdAndClass, storageAvailable } from '../js_components/helperFunctions'
 
 let dashboard = new Dashboard()
 let project = new Project()
@@ -8,8 +8,7 @@ let project = new Project()
 export function initializeDOM() {
     // const addButton = document.querySelector("#add-btn")
     // addButton.addEventListener("click", () => {
-    //     dashboard.addProject()
-    //     alert("added")
+    //     alertHi()
     // })
 
     // show projects and todos on the dashboard
@@ -21,5 +20,21 @@ export function initializeDOM() {
     // if add todo button is pressed inside a particular project, create new todo
         // todo creation form displays
         // project.todos is updated
+
+    function displayProjects() {
+
+    }
+
+    function displayTodo() {
+
+    }
+
+    function saveProject() {
+        if (storageAvailable("localStorage")) {
+            // yay we can use local storage
+        } else {
+            // no we cant use local storage!
+        }
+    }
 
 }
